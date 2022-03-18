d598a612eb8189aa51ee66a64fbfe1f9b0dfc27e
Ahrmerd <Ahrmerd@gmail.com>
2022-03-18 22:05:30 +0100

    withRange function fetched greater than the limit. fix withRange function to fetch within the limit

--- app/Traits/HasRange.php
+++ app/Traits/HasRange.php
@@ -17,7 +17,7 @@ trait HasRange
         if (is_string(request('range')) && json_decode(request('range')) && count(json_decode(request('range'))) == 2) {
             $range = json_decode(request('range'));
             $offset = $range[0];
-            $limit = $range[1] - $range[0] + 1;
+            $limit = $range[1] - $range[0];
             return $query->offset($offset)->limit($limit);
         };
         return $query;
